40699635dbab9ce79c0278275f4b1cd19db6ed98
Ben Standefer <benstandefer@gmail.com>
2018-08-08 15:32:49 -0700

    Custom JavaScript errors with Babel

--- app/errors.js
+++ app/errors.js
@@ -1,7 +1,26 @@
-export class ImportedFooError extends Error {}
+/**
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error#Custom_Error_Types
+ *  WARN: Heed the note in the "ES6 Custom Error Class" section:
+ *  > Babel and other transpilers will not correctly handle the following code
+ *  > without additional configuration (https://github.com/loganfsmyth/babel-plugin-transform-builtin-extend)
+ *
+ * In addition to enabling babel-plugin-transform-builtin-extend, you also need
+ * to set __proto__ on each custom error.
+ * https://github.com/babel/babel/issues/4485#issuecomment-377970736
+ */
+
+export class ImportedFooError extends Error {
+  constructor(message) {
+    super(message);
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, ImportedFooError);
+    }
+    this.constructor = ImportedFooError;
+    this.__proto__   = ImportedFooError.prototype;
+  }
+}
 
 console.log(`Without try/catching?`);
-console.log(new ImportedFooError instanceof Error);
 
 try {
   throw new ImportedFooError(`We threw ImportedFooError (same file)`);
